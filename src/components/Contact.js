471111a1e14109c8452a2d35093c31bcce632ed3
Mrinmay Ray <mreenmayray@gmail.com>
2023-08-14 15:13:34 +0530

    Fix -Contack Form Fixed(Proper Working)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,46 +1,28 @@
-import { useState } from "react";
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'; // Updated import
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Contact = () => {
-  const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
-  const [status, setStatus] = useState({});
 
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
+  const form = useRef();
 
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-    }
+
+    emailjs.sendForm('service_qfa96ma', 'template_2iqg144', form.current, 'qqEgltz7EgyEK34aX')
+      .then((result) => {
+        console.log(result.text);
+        toast.success("Message Sent"); // Show success notification
+        form.current.reset();
+      }, (error) => {
+        console.log(error.text);
+        toast.error("Sending failed. Please try again!"); // Show error notification
+      });
   };
 
   return (
@@ -50,7 +32,7 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
               }
             </TrackVisibility>
           </Col>
@@ -58,38 +40,33 @@ export const Contact = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Get In Touch</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                  <h2>Get In Touch</h2>
+                  <form ref={form} onSubmit={sendEmail}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="text" placeholder="First Name" name="first_name" required />
                       </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="text" placeholder="Last Name" name="last_name" />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="email" placeholder="Email Address" name="user_email" required pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}" title="Please enter a valid email address" />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="tel" placeholder="Phone No." name="user_phone" required pattern="[0-9]*" title="Please enter only numeric characters" />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea rows="6" placeholder="Message" name="message" required></textarea>
+                        <button type="submit" value="send"><span>Send</span></button>
+                      </Col>
+                    </Row>
+                  </form>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
+      <ToastContainer /> {/* Add ToastContainer at the end */}
     </section>
   )
 }
